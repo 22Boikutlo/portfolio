64a1e2a69e0bb0675d808b6031dc82126b34a1e8
22Boikutlo <boikutlos@gmail.com>
2025-05-20 14:26:21 +0200

    new app

---
+++ pages/project/projects.js
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import "../../public/styles/global/styles.css"
+
+const projects = () => {
+  return (
+    <>
+    <Button variant="contained" color="primary">Test    </Button>
+
+    </>
+    
+  )
+}
+
+export default projects
