3abc88a8f63355e298dcfcfdf50214499b5ff4ba
22Boikutlo <boikutlos@gmail.com>
2025-09-03 15:54:13 +0200

    changged module components cto global css

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import styles from './Hero.module.css'
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-<div className={styles.heroContainer}>
-      <div className={styles.heroText}>
-        <h1>Software Developer</h1>
-         </div>
-    </div>
+    <div className='hero'>
+      <div className='heroContainer'>
+        <div className='heroText'>
+          <h1>Software Developer</h1>
+        </div>
+      </div>
     </div>
   )
 }
