37cdb66e9f36254f3969e083bccee019d88d2fd5
22Boikutlo <boikutlos@gmail.com>
2025-09-05 11:26:57 +0200

    fixed navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
-import { LogoDev, Menu } from '@mui/icons-material';
 import { Link } from 'react-scroll';
+import logo from './assets/logo.png';
+import menubar from './assets/menu.png';
+import contact from './assets/contact.png';
+import Image from 'next/image';
 
 const Navbar = () => {
   /* keeping navigation bar transparent until scrolled more than 50px */
@@ -20,24 +23,23 @@ const Navbar = () => {
 
   return (
             <nav className='navbar'>
-            <img src={logo} alt="logo" className='logo' />
+            <Image src={logo} alt="logo" className='logo' />
             <div class="desktopMenu">
-                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home </Link>
-                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>About </Link>                
-                <Link activeClass='active' to='stack' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>TechStack </Link>
-                <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Portfolio </Link>
-                <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Client </Link>
+                <Link activeClass='active' to='aboutme' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>About Me </Link>
+                <Link activeClass='active' to='education' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Education </Link>                
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Skills </Link>
+                <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Projects </Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Contact </Link>
             </div>
             <button className='desktopMenuBtn' onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })} >
-                <img src={contact} alt='contact' className='desktopMenuImg ' />Contact Me
+                <Image src={contact} alt='contact' className='desktopMenuImg ' />Contact Me
             </button>
-            <img src={menubar} alt="menu" className='mobileMenu' onClick={() => setShowMenu(!showMenu)} />
+            <Image src={menubar} alt="menu" className='mobileMenu' onClick={() => setShowMenu(!showMenu)} />
             <div class="navMenu" style={{ display: showMenu ? 'flex' : 'none' }}>
-                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Home </Link>
-                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>About </Link>
-                <Link activeClass='active' to='stack' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>My Tech Stack </Link>
-                <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Portfolio </Link>
-                <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Client </Link>
+                <Link activeClass='active' to='aboutme' spy={true} smooth={true} offset={-100} duration={500} className='listItem' onClick={() => setShowMenu(false)}>About Me </Link>
+                <Link activeClass='active' to='education' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Education</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Skills </Link>
+                <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Projects </Link>
                 <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Contact </Link>
 
             </div>
